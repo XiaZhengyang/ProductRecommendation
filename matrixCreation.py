39159efa617405a19496fdd5fa16dda3ef69bc15
SamuelZhouHe <hezhou556@gmail.com>
2016-07-06 13:53:13 +0800

    Finish the information matrix

--- matrixCreation.py
+++ matrixCreation.py
@@ -8,7 +8,6 @@ with open('../clientInformation.json') as data_file:
 
 infoMatrix = np.zeros((4,7),int)
 
-infoMatrix[3,6]=5
 
 for i in range (0,4):
 	birthDay= data["clients"][i]["customerBaseExt"]["birthday"]
@@ -16,9 +15,26 @@ for i in range (0,4):
 	infoMatrix[i,0] = 2016- birthYear
 	infoMatrix[i,1] =int(data["clients"][i]["customerBaseExt"]["householdRegisterAddrCity"])
 	if data["clients"][i]["customerMarriage"]["isMarried"] == "Married":
-		infoMatrix[i,2] ="1"
+		infoMatrix[i,2] =1
 	else:
-		infoMatrix[i,3] = "0"
+		infoMatrix[i,2] = 0
+	educationString = data["clients"][i]["customerEducations"][0]["education"]
 
+	if educationString == "Bachelor":
+		infoMatrix[i,3] = 4
+	elif educationString =="College":
+		infoMatrix[i,3] = 3
+	elif educationString == "Senior":
+		infoMatrix[i,3] = 2
+	else:
+		infoMatrix[i,3] = 1
+
+	assetIncome = data["clients"][i]["customerAssetsIncome"]["monthlySalary"] + data["clients"][i]["customerAssetsIncome"]["monthlyIncomeOther"] - data["clients"][i]["customerAssetsIncome"]["monthlyExpense"]
+	infoMatrix[i,4] = assetIncome;
+
+	infoMatrix[i,5] = data["clients"][i]["customerAssetsIncome"]["dependentNumber"]
+
+	if data["clients"][i]["customerAssetsVehicles"]:
+		infoMatrix[i,6] = data["clients"][i]["customerAssetsVehicles"][0]["vehiclePurchasePrice"]
 print infoMatrix
 
