84f1ddec106ba473704ff31f31aedf93651f9b41
XiaZhengyang <XiaZhengyang@XiadeMacBook-Pro.local>
2016-07-13 15:19:35 +0800

    Integrated Input Process

--- productRecommendation.py
+++ productRecommendation.py
@@ -8,9 +8,9 @@ from scipy.cluster.vq import kmeans2
 from scipy.optimize import minimize
 from numpy import size
 import numpy.matlib
-numpy.set_printoptions(threshold='nan')
+numpy.set_printoptions(precision=1,threshold=1000000)
 
-with open('../申请客户信息.csv', encoding='gbk') as csvfile:
+with open('申请客户信息.csv', encoding='gbk') as csvfile:
 	reader = csv.reader(csvfile, delimiter=',', quotechar='"')
 	next(reader)
 	data = []
@@ -19,8 +19,9 @@ with open('../申请客户信息.csv', encoding='gbk') as csvfile:
 		if not line in data:
 			data.append(line)
 			numSample += 1
-numFeature = 45
+numFeature = 100
 infoMatrix = numpy.zeros((numSample, numFeature))
+label = numpy.zeros(13)
 for i in range(numSample):
 	if data[i][4] == '信优贷':
 		infoMatrix[i][0] = 1
@@ -32,12 +33,10 @@ for i in range(numSample):
 		infoMatrix[i][3] = 1
 	if data[i][5] == 24:
 		infoMatrix[i][4] = 1
-	elif data[i][5] == 36:
-		infoMatrix[i][5] = 1
 	elif data[i][5] == 48:
-		infoMatrix[i][6] = 1
-	else:
 		infoMatrix[i][7] = 1
+	else:
+		infoMatrix[i][6] = 1
 	infoMatrix[i][8] = data[i][6]
 	if data[i][7] == '消费':
 		infoMatrix[i][8] = 1
@@ -47,29 +46,49 @@ for i in range(numSample):
 		infoMatrix[i][10] = 1
 	else:
 		infoMatrix[i][11] = 1
-	#infoMatrix[i][12] = float(data[i][8])
+	try:
+		infoMatrix[i][12] = float(data[i][8])
+	except:
+		infoMatrix[i][12] = 0
 	if data[i][9] == '是':
 		infoMatrix[i][13] = 1
-
+	if data[i][17] == '无按揭购房':
+		infoMatrix[i][14] = 1
+	elif data[i][17] == '商业按揭房':
+		infoMatrix[i][15] = 1
+	elif data[i][17] == '公积金按揭房':
+		infoMatrix[i][16] = 1
+	elif data[i][17] == '自建房':
+		infoMatrix[i][17] = 1
+	elif data[i][17] == '单位住房':
+		infoMatrix[i][18] = 1
+	elif data[i][17] == '亲属住房':
+		infoMatrix[i][19] = 1
+	else:
+		infoMatrix[i][20] = 1
+	try:
+		infoMatrix[i][21] = float(data[i][18])
+	except:
+		infoMatrix[i][21] = 0
 
 	#Education background columns
 	if data[i][19]=='大学本科':
 		infoMatrix[i][22] = 1
-	if data[i][19]=='高中及中专':
+	elif data[i][19]=='高中及中专':
 		infoMatrix[i][23] = 1
-	if data[i][19]=='大专':
+	elif data[i][19]=='大专':
 		infoMatrix[i][24] = 1
-	if data[i][19]=='硕士':
+	elif data[i][19]=='硕士':
 		infoMatrix[i][25]=1
-	if data[i][19]=='初中及以下':
+	else:
 		infoMatrix[26] = 1
 
 	#Maritial status
 	if data[i][21]=='已婚':
 		infoMatrix[i][27]=1
-	if data[i][21]=='未婚':
+	elif data[i][21]=='未婚':
 		infoMatrix[i][28]=1
-	if data[i][21]=='离异':
+	else:
 		infoMatrix[i][29]=1
 
 	#30.Gender 
@@ -86,106 +105,46 @@ for i in range(numSample):
 	#32-37Job and working company
 	if data[i][31]=='一般正式员工':
 		infoMatrix[i][32]=1
-	if data[i][31]=='中级管理人员':
+	elif data[i][31]=='中级管理人员':
 		infoMatrix[i][33]=1
-	if data[i][31]=='一般管理人员':
+	elif data[i][31]=='一般管理人员':
 		infoMatrix[i][34]=1
-	if data[i][31]=='派遣员工':
+	elif data[i][31]=='派遣员工':
 		infoMatrix[i][35]=1
-	if data[i][31]=='高级管理人员':
+	elif data[i][31]=='高级管理人员':
 		infoMatrix[i][36]=1
-	if data[i][31]=='负责人':
+	else:
 		infoMatrix[i][37]=1
 
 
 	#38-44	Type of working unit
 	if data[i][32]=='机关事业单位':
 		infoMatrix[i][38]=1
-	if data[i][32]=='外资企业':
+	elif data[i][32]=='外资企业':
 		infoMatrix[i][39]=1
-	if data[i][32]=='私营企业':
+	elif data[i][32]=='私营企业':
 		infoMatrix[i][40]=1
-	if data[i][32]=='国有股份':
+	elif data[i][32]=='国有股份':
 		infoMatrix[i][41]=1
-	if data[i][32]=='合资企业':
+	elif data[i][32]=='合资企业':
 		infoMatrix[i][42]=1
-	if data[i][32]=='民营企业':
+	elif data[i][32]=='民营企业':
 		infoMatrix[i][43]=1
-	if data[i][32]=='个体':
+	else:
 		infoMatrix[i][44]=1
 
+print(infoMatrix)
 
 
-print (infoMatrix)
-	
 
-
-'''sampleNum = size(data["clients"])
-infoMatrix = numpy.zeros((sampleNum,7), float)
-validSampleNum = 0
-numFeature = 7
-numCluster = 6
+'''numCluster = 6
 iter = 1000
 alpha = 0.1
 
-for i in range(sampleNum):
-	if "customerBaseExt" in data["clients"][i]:
-		validSampleNum += 1
-		birthDay = data["clients"][i]["customerBaseExt"]["birthday"]
-		birthYear = int(birthDay[0:4])
-		infoMatrix[i,0] = 1916 - birthYear
 
-		gender = data["clients"][i]["customerBaseExt"]["gender"]
-		if gender == "Male":
-			infoMatrix[i,1] = 0
-		else:
-			infoMatrix[i,1] = 1
-
-		if data["clients"][i]["customerMarriage"]["isMarried"] == "Married":
-			infoMatrix[i,2] = 0
-		else:
-			infoMatrix[i,2] = 1
-
-		educationString = data["clients"][i]["customerEducations"][0]["education"]
-		if educationString == "Bachelor":
-			infoMatrix[i,3] = 4
-		elif educationString == "College":
-			infoMatrix[i,3] = 3
-		elif educationString == "Senior":
-			infoMatrix[i,3] = 2
-		else:
-			infoMatrix[i,3] = 1
-
-		totalMonthlyIncome = data["clients"][i]["customerAssetsIncome"]["monthlySalary"] + data["clients"][i]["customerAssetsIncome"]["monthlyIncomeOther"]
-		if totalMonthlyIncome * 12 > data["clients"][i]["customerAssetsIncome"]["yearlyIncome"]:
-			assetIncome = totalMonthlyIncome - data["clients"][i]["customerAssetsIncome"]["monthlyExpense"]
-		else:
-			assetIncome = data["clients"][i]["customerAssetsIncome"]["yearlyIncome"] / 12 - data["clients"][i]["customerAssetsIncome"]["monthlyExpense"]
-		#assetIncome = data["clients"][i]["customerAssetsIncome"]["monthlySalary"] + data["clients"][i]["customerAssetsIncome"]["monthlyIncomeOther"] - data["clients"][i]["customerAssetsIncome"]["monthlyExpense"]
-		infoMatrix[i,4] = assetIncome
-
-		infoMatrix[i,5] = data["clients"][i]["customerAssetsIncome"]["dependentNumber"]
-
-	#print(data["clients"][i])
-
-		if data["clients"][i]["customerAssetsVehicles"]:
-			infoMatrix[i,6] = data["clients"][i]["customerAssetsVehicles"][0]["vehiclePurchasePrice"]
-
-	#infoMatrix[i,1] =int(data["clients"][i]["customerBaseExt"]["householdRegisterAddrCity"])
-
-
-
-validInfoMatrix = numpy.zeros((validSampleNum, 7), float)
-j = 0
-for i in range(sampleNum):
-	if infoMatrix[i][0] != 0:
-		validInfoMatrix[j] = infoMatrix[i]
-		j += 1
-
-stdInfoMatrix = whiten(validInfoMatrix)
-label = numpy.zeros(validSampleNum, int)
+stdInfoMatrix = whiten(infoMatrix)
 random.seed(version=2)
-prototype = random.sample(range(validSampleNum), numCluster)
+prototype = random.sample(range(sampleNum), numCluster)
 centroids = stdInfoMatrix[prototype]
 ptLabel = label[prototype]
 
